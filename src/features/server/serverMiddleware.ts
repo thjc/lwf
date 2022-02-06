3c042607d797ecd6540dc26e3f42ba9399048700
Toby Collett <thjc@plan9.net.nz>
2022-02-06 20:47:24 +1300

    Handle socket disconnect

--- src/features/server/serverMiddleware.ts
+++ src/features/server/serverMiddleware.ts
@@ -8,16 +8,13 @@ const ServerMiddleware: Middleware = store => {
   return next => action => {
     const isConnectionEstablished = (socket != null) && store.getState().server.isConnected
 
-    console.log('middleware', action, socket)
-
-    if (socket === undefined) {
-      const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
+    if (socket === undefined || socket.readyState === socket.CLOSED) {
+      const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
 
       socket = new WebSocket(protocol + '://' + window.location.host + '/gamestate')
       if (socket !== undefined) {
         socket.onopen = () => {
           if (socket != null) {
-            console.log('Connected')
             for (const msg of store.getState().server.messages) {
               socket.send(JSON.stringify(msg))
             }
@@ -31,26 +28,29 @@ const ServerMiddleware: Middleware = store => {
         }
 
         socket.onmessage = (event: MessageEvent) => {
-          console.log(event)
           const msg = JSON.parse(String(event.data))
           if (msg.cmd === 'update') {
             const gameState = JSON.parse(atob(msg.data))
             store.dispatch(loadGame(gameState))
           }
         }
+
+        socket.onclose = () => {
+          setTimeout(() => (store.dispatch(serverActions.disconnected())), 1000);
+        }
       }
     }
 
     const sendOrQueue = (cmd: string, data: string) => {
       const message = { cmd: cmd, data: data }
-      if ((socket != null) && isConnectionEstablished) {
+      if (socket && socket.readyState === socket.OPEN && isConnectionEstablished) {
         socket.send(JSON.stringify(message))
       } else {
         store.dispatch(serverActions.queueMessage(message))
       }
     }
 
-    if (socket && isConnectionEstablished) {
+    if (socket && socket.readyState === socket.OPEN && isConnectionEstablished) {
       if (serverActions.sendGameState.match(action)) {
         const stateString = JSON.stringify(store.getState())
         const encodedState = btoa(stateString !== undefined ? stateString : '')
--- src/features/server/serverSlice.ts
+++ src/features/server/serverSlice.ts
@@ -34,6 +34,10 @@ export const serverSlice = createSlice({
     },
     sendGameState: (state) => {
 
+    },
+    disconnected: (state) => {
+      state.isConnected = false;
+      state.isEstablishingConnection = false;
     }
   }
 })
