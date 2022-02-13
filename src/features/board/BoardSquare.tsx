a261efce3ed1d588423ba98da624952e8ce100df
Toby Collett <thjc@plan9.net.nz>
2022-02-13 21:52:10 +1300

    Fix use of canPlayOnBoard in memoized canDrop function

--- src/features/board/BoardSquare.tsx
+++ src/features/board/BoardSquare.tsx
@@ -13,7 +13,7 @@ import styles from './Board.module.css'
 import { useDrag, useDrop } from 'react-dnd'
 import { Box } from '@mui/material'
 
-export function BoardSquare (props: any) {
+export function BoardSquare(props: any) {
   const dispatch = useAppDispatch()
 
   const tileValue = props.tile
@@ -24,12 +24,12 @@ export function BoardSquare (props: any) {
       accept: 'tile',
       drop: (item: any) => dispatch(placeWorkingTile({ value: item.value, from: item.from, place: props.position, dropType: tileValue.state })),
       canDrop: (item, monitor) => {
-          return tileValue.state === SquareState.Hand ||
+        return tileValue.state === SquareState.Hand ||
           tileValue.state === SquareState.HandEnd ||
-          (canPlayOnBoard && tileValue.state === SquareState.Empty)
-        },
+          tileValue.state === SquareState.Empty
+      },
       collect: (monitor) => ({
-        isOver: !!monitor.isOver()
+        isOver: !!monitor.isOver(),
       })
     }),
     [tileValue]
@@ -43,7 +43,7 @@ export function BoardSquare (props: any) {
       isDragging: !!monitor.isDragging()
     })
   }),
-  [tileValue]
+    [tileValue]
   )
 
   if (tileValue.state !== SquareState.Empty && tileValue.state !== SquareState.HandEnd) {
@@ -58,14 +58,14 @@ export function BoardSquare (props: any) {
     if (tileValue.state !== SquareState.HandEnd) {
       style = `${styles.boardSquare} ${((v: SquareType) => {
         switch (v) {
-          case (SquareType.Plain): { return ""}
-          case (SquareType.TripleWord): { return styles.boardTripleWord}
-          case (SquareType.TripleLetter): { return styles.boardTripleLetter}
-          case (SquareType.DoubleWord): { return styles.boardDoubleWord}
-          case (SquareType.DoubleLetter): { return styles.boardDoubleLetter}
+          case (SquareType.Plain): { return "" }
+          case (SquareType.TripleWord): { return styles.boardTripleWord }
+          case (SquareType.TripleLetter): { return styles.boardTripleLetter }
+          case (SquareType.DoubleWord): { return styles.boardDoubleWord }
+          case (SquareType.DoubleLetter): { return styles.boardDoubleLetter }
         }
       })(type)}`
     }
-    return (<Box ref={drop} className={style} />)
+    return (<Box ref={canPlayOnBoard ? drop : undefined} className={style} />)
   }
 }
